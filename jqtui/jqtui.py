4c426d7334bc6e74043c026d5bda76cf5a74a7ba
Solomon Huang <huang.solomon@gmail.com>
2023-08-26 14:18:14 -0700

    add footer, and error toggle

--- jqtui/jqtui.py
+++ jqtui/jqtui.py
@@ -6,8 +6,9 @@ import typer
 from pyjq import ScriptRuntimeError
 from rich.syntax import Syntax
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.containers import VerticalScroll
-from textual.widgets import Input, Markdown, Static
+from textual.widgets import Footer, Input, Static
 from typing_extensions import Annotated
 
 typer_app = typer.Typer(no_args_is_help=True, add_completion=False)
@@ -21,28 +22,51 @@ class ResultHeader(Static):
     """Display a json."""
 
 
+class Message(Static):
+    """Display messages"""
+
+    def on_mount(self):
+        self.update('Please enter a jq expression above')
+
+
 class JQTUI(App):
     CSS_PATH = 'jqtui.css'
+    BINDINGS = [
+        Binding(key="ctrl+c", action="quit", description="Quit", priority=True),
+        Binding(
+            key="ctrl+t",
+            action="toggle_errors",
+            description="Show/Hide Errors",
+            priority=True,
+        ),
+    ]
 
-    def __init__(self, filename, *args, **kwargs):
+    def __init__(self, filename):
         self.filename = filename
-        super().__init__(*args, **kwargs)
+        super().__init__()
 
         with open(filename) as f:
             self.data = json.loads(f.read())
 
+    def action_toggle_errors(self):
+        if self.query_one('#message').has_class('hide'):
+            self.query_one('#message').remove_class('hide')
+        else:
+            self.query_one('#message').add_class('hide')
+
     def compose(self) -> ComposeResult:
         yield Input(id="input", placeholder="Type in a jq command")
-        yield Static(id="message")
+        yield Message(id="message", classes="hide")
+
         with VerticalScroll(id="results-container"):
             yield ResultHeader(id="results-header")
             yield Result(id="results")
-            yield Markdown(id="md")
+        yield Footer()
 
     def on_mount(self) -> None:
         """Called when app starts."""
         # Give the input focus, so we can start typing straight away
-        self.query_one(Input).focus()
+        # self.query_one(Input).focus()
         syntax = self.get_syntax([self.data])
         self.query_one("#results").update(syntax)
         self.query_one("#results-header").update('Displaying original file contents')
@@ -61,18 +85,20 @@ class JQTUI(App):
         try:
             filtered_data = pyjq.all(value, self.data)
             if not any(filtered_data):
-                raise ValueError('The results produced all Nones')
+                raise ValueError('The query did not produce any results')
             syntax = self.get_syntax(items=filtered_data)
 
             self.query_one("#results-header").update(
                 f"Displaying results for: '{value}'"
             )
             self.query_one("#results").update(syntax)
-            self.query_one("#message").update("")
             self.query_one('#input').remove_class('red_border')
+            self.query_one("#message").update(
+                'Congrats! There are no errors with the jq syntax'
+            )
         except (ValueError, ScriptRuntimeError) as e:
             self.query_one('#input').add_class('red_border')
-            self.query_one("#message").update(f"Not a valid jq query: {value}: {e}")
+            self.query_one("#message").update(f"ERROR: {e}")
 
     def get_syntax(self, items: list[Any]):
         """
